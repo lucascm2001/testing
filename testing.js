64956cf1a61c4facb8dd5e79b59cad984f294ee6
Lucas Chaves Meyles <lchavesmeyles@gmail.com>
2024-03-05 19:05:56 -0800

    Finished project

--- testing.js
+++ testing.js
@@ -17,9 +17,30 @@ const calculator = {
   divide: (a, b) => a / b,
 };
 
+function caesarCipher(str, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return str.split('')
+    .map((letter) => {
+      if (alphabet.includes(letter)) {
+        const index = alphabet.indexOf(letter);
+        return alphabet[(index + shift) % 26];
+      }
+      return letter;
+    })
+    .join('');
+}
+
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((acc, curr) => acc + curr) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
+
 module.exports = {
-  sum, capitalize, reverseString, calculator,
+  sum, capitalize, reverseString, calculator, caesarCipher, analyzeArray,
 };
 
-const string = 'hey';
-console.log(string.split('').reverse().join(''));
+// const string = 'hey';
--- testing.test.js
+++ testing.test.js
@@ -29,3 +29,31 @@ test('multiplies 10 and 2 to make 20', () => {
 test('divides 3 by 9 to make 3', () => {
   expect(testing.calculator.divide(9, 3)).toBe(3);
 });
+
+test('cipher on bonk to eron with shift of 3', () => {
+  expect(testing.caesarCipher('bonk', 3)).toBe('erqn');
+});
+
+test('cipher on zyx to azy with shift of 1', () => {
+  expect(testing.caesarCipher('zyx', 1)).toBe('azy');
+});
+
+test('cipher on zy.!x to az.!y with shift of 0', () => {
+  expect(testing.caesarCipher('zy.!x', 0)).toBe('zy.!x');
+});
+
+test('average of 1, 2, 3 is 2', () => {
+  expect(testing.analyzeArray([1, 2, 3]).average).toBe(2);
+});
+
+test('min of 1, 2, 3 is 1', () => {
+  expect(testing.analyzeArray([1, 2, 3]).min).toBe(1);
+});
+
+test('max of 1, 2, 3 is 3', () => {
+  expect(testing.analyzeArray([1, 2, 3]).max).toBe(3);
+});
+
+test('length of 1, 2, 3 is 3', () => {
+  expect(testing.analyzeArray([1, 2, 3]).length).toBe(3);
+});
